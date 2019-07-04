2f70ad0bc1b6bf9ff38e8c41cb42e227e437096b
dainguyenblabla <dainguyenblabla@gmail.com>
2019-07-05 00:49:57 +0700

    chapter10

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem "sqlite3"
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
 gem "bcrypt"
+gem "faker", "1.7.3"
+gem "will_paginate",           "3.1.7"
+gem "bootstrap-will_paginate", "1.0.0"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit,:update, :index, :destroy]
+  before_action :correct_user, only:[:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted!"
+    redirect_to users_url
+  end
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def new
   	@user = User.new
   end
 
   def show
   	@user = User.find params[:id]
+  end
+
+  def edit
+    @user = User.find params[:id]
   end
 
   def create
@@ -16,6 +38,29 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless @user == current_user
   end
 
   private
